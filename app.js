5be91ff63c466ea166bb380b329bfdf73f6d82e9
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2023-07-30 12:14:35 +0300

    addTodo function created

--- app.js
+++ app.js
@@ -2,9 +2,26 @@
 const inputBox = document.querySelector('#input-box');
 
 // we will add list items to this container
-const listContainer = document.querySelector('#list-container');
+const listContainer = document.querySelector('.list-container');
 
 const addBtn = document.querySelector('#add-btn');
 
 // add event listener
 addBtn.addEventListener('click', addTodo);
+
+function addTodo(e) {
+  e.preventDefault();
+  if (!inputBox.value) {
+    return;
+  } else {
+    const liEl = document.createElement('li');
+    liEl.innerText = inputBox.value;
+    listContainer.appendChild(liEl);
+
+    const spanEl = document.createElement('span');
+    spanEl.innerHTML = '\u00d7';
+
+    liEl.appendChild(spanEl);
+  }
+  inputBox.value = '';
+}
