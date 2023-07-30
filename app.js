a41b3be46c07d934047b0bb0c1c246aa8bf14a9c
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2023-07-30 11:48:36 +0300

    variables created

---
+++ app.js
@@ -0,0 +1,7 @@
+// take inputBox
+const inputBox = document.querySelector('#input-box');
+
+// we will add list items to this container
+const listContainer = document.querySelector('#list-container');
+
+const addBtn = document.querySelector('#add-btn');
